c697244d8131ee6f849dce2605022ac4ea3293f7
SteffenOldenburg <58236670+SteffenOldenburg@users.noreply.github.com>
2020-03-10 23:17:58 +0100

    Update Year 2020

--- server.js
+++ server.js
@@ -461,7 +461,7 @@ router.route('/pay-stripe/:auth_key/:member_id')
 			  amount: amount * 100,
 			  currency: "chf",
 			  source: stripe_token,
-			  description: "Mitgliederbeitrag - " + member_id,
+			  description: "Mitgliederbeitrag 2020 " + member_id,
 			  receipt_email: stripe_email,
 			}).then(function(charge) {
 			  console.log("Charge created");
